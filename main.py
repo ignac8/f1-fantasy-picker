f8e20407b8b4086caad5c4074c4de7b1ab75a4da
ignac8 <ignac8@op.pl>
2022-05-26 16:29:07 +0200

    Added required drivers

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ import itertools
 import requests
 
 
-def budget_filter(combination, budget):
+def budget_filter(combination, budget, required_drivers):
     driver_price = sum(map(lambda driver: driver['price'], combination[0]))
     constructor_price = combination[1]['price']
     is_budget_right = driver_price + constructor_price < budget
     is_turbo_okay = combination[0][combination[2]]['price'] < 20
     is_mega_okay = combination[0][combination[3]] != combination[0][combination[2]]
-    return is_budget_right and is_turbo_okay and is_mega_okay
+    has_required_drivers = all(map(lambda required_driver:
+                                   any(map(lambda x: x['last_name'] == required_driver, combination[0])),
+                                   required_drivers))
+    return is_budget_right and is_turbo_okay and is_mega_okay and has_required_drivers
 
 
 def calculate_points(combination, with_mega_driver):
@@ -32,16 +35,17 @@ def get_points_for_player(driver, turbo_driver_num, mega_driver_num, with_mega_d
 
 
 if __name__ == '__main__':
-    budget = 102.9
+    budget = 101.5
     number_of_drivers = 5
     with_mega_driver = True
+    required_drivers = ['Verstappen', 'Leclerc']
     json = requests.get("https://fantasy-api.formula1.com/f1/2022/players").json()['players']
     drivers = list(filter(lambda x: x['position'] == 'Driver', json))
     constructors = list(filter(lambda x: x['position'] == 'Constructor', json))
     all_combinations = itertools.product(
         itertools.combinations(drivers, number_of_drivers), constructors, range(number_of_drivers),
         range(number_of_drivers))
-    possible_combinations = filter(lambda x: budget_filter(x, budget), all_combinations)
+    possible_combinations = filter(lambda x: budget_filter(x, budget, required_drivers), all_combinations)
     combinations_with_points = list(map(lambda x: calculate_points(x, with_mega_driver), possible_combinations))
     best_combination = max(combinations_with_points, key=lambda x: x[4])
     print(best_combination)
